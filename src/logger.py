068d486824633800ad96f4bef5f911d97d4b1dbd
Alex <alex_lanes@hotmail.com>
2023-09-26 11:04:32 -0300

    Job para limpar logs antigos

--- src/logger.py
+++ src/logger.py
@@ -4,15 +4,17 @@ from sys import exc_info
 # interno
 from src.util import *
 
+ENCODING = "utf-8"
 NOME_ARQUIVO = "execução.log"
+FORMATO_DATA = "%Y-%m-%dT%H:%M:%S"
 
 class Logger:
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s | id(%(process)d) | level(%(levelname)s) | %(message)s",
-        datefmt="%Y-%m-%dT%H:%M:%S",
+        datefmt=FORMATO_DATA,
         filename=NOME_ARQUIVO,
-        encoding="utf-8",
+        encoding=ENCODING,
         filemode="a"
     )
     
@@ -32,3 +34,32 @@ class Logger:
 __all__ = [
     "Logger"
 ]
+
+"""Remover linhas de log acima do `TEMPO_MAXIMO`
+Executar como `python -m src.logger` na pasta raiz"""
+if __name__ == "__main__":
+    import re
+    from datetime import datetime, timedelta
+    TEMPO_MAXIMO: timedelta = timedelta(hours=11)
+    Logger.informar("Buscando por Logs que estão acima do tempo máximo configurado")
+
+    agora = datetime.now()
+    linhasParaRemover: int = 0
+    with open(NOME_ARQUIVO, "r+", encoding=ENCODING) as log:
+        linhas: list[str] = log.readlines()
+        for linha in linhas:
+            dataHora = re.search(r"^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}", linha)
+            if not dataHora: 
+                linhasParaRemover = linhasParaRemover + 1
+                continue
+            dataHora = datetime.strptime(dataHora.group(), FORMATO_DATA)
+            if dataHora + TEMPO_MAXIMO < agora: 
+                linhasParaRemover = linhasParaRemover + 1
+            else: break
+        
+        if linhasParaRemover > 0:
+            log.seek(0)     # mover cursor pro começo do arquivo
+            log.truncate()  # limpar arquivo
+            log.writelines(linhas[linhasParaRemover:]) # linhas fora do tempo máximo
+        
+    Logger.informar(f"{ 'Nenhuma linha removida' if linhasParaRemover == 0 else f'Removido { linhasParaRemover } linhas' } do arquivo '{ NOME_ARQUIVO }' após o tempo máximo '{ TEMPO_MAXIMO }'")
